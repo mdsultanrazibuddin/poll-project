72d05bce37998d55a6edc5b6b39e721b9f8cab68
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2024-07-06 14:53:20 +0600

    'added'

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ app.use(express.json())
 
 app.get('/', (req, res) =>{
     res.json({
-        message:'i am ajaira'
+        message:'i am route'
     })
 })
 
 mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+app.listen(4545, () =>{
+            console.log('port 4545');
+        })
